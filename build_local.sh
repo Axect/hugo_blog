ba57845a1ccb209b8ff1ddca98ed2af4a3ebb32d
axect <axect.tg@proton.me>
2026-04-06 03:49:05 +0800

    feat: update build script with stats fetch, fix schema.org detection, remove About pages, update gitignore

--- build_local.sh
+++ build_local.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Fetch latest software stats (stars, downloads)
+python3 scripts/fetch_stats.py
+
+# Build Hugo site
 rm -rf ../axect.github.io/*
 rm -rf public/
 hugo -t hello-friend-ng
